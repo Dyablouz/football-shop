7252f3348fb3e3e138f02f4cfb00dc8724418a65
Dyablouz <fernando123lawrence@gmail.com>
2025-09-10 09:23:21 +0700

    Tugas 2

--- main/models.py
+++ main/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
     thumbnail = models.URLField()
     category = models.CharField(max_length=100)
     is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+            return self.name
--- main/views.py
+++ main/views.py
@@ -3,10 +3,8 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'app_name': 'Football Shop',
-        'category' : 'Clothes',
-        'name': 'Baju',
-        'description': 'Keren',
-        'thumbnail': ''
+        'nama' : 'Fernando Lawrence',
+        'kelas': 'PBP B',
     }
 
     return render(request, "main.html", context)
